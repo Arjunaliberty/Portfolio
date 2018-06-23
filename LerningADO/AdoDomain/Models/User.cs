b2c9414d99f2fb9ccf55de985e218cccffda9534
ArjunaLiberty <arjunaliberty@gmail.com>
2018-06-23 17:49:28 +0300

    Добавлен класс модели для пользователя

---
+++ LerningADO/AdoDomain/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoDomain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Login { get; set; }
+        public int Password { get; set; }
+        public int Role { get; set; }
+    }
+}
