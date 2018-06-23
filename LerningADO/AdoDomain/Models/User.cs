fa322b9b0f9c54e9b660725f4195e2acc66f3989
ArjunaLiberty <arjunaliberty@gmail.com>
2018-06-23 19:34:04 +0300

    Добавлены атрибуты для модели User

--- LerningADO/AdoDomain/Models/User.cs
+++ LerningADO/AdoDomain/Models/User.cs
@@ -1,18 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace AdoDomain.Models
 {
     public class User
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Поле не может быть пустым")]
+        [Display(Name ="Имя")]
+        [RegularExpression(@"^[A-zА-Я]+$", ErrorMessage ="Некорректный ввод")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage ="Длина строки должна быть от 3 до 50 символов")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Поле не может быть пустым")]
+        [Display(Name = "Фамилия")]
+        [RegularExpression(@"^[A-zА-Я]+$", ErrorMessage = "Некорректный ввод")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 50 символов")]
         public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Поле не может быть пустым")]
+        [Display(Name = "Логин")]
+        [RegularExpression(@"^[A-zА-я0-9_]+$", ErrorMessage ="Некорректный ввод")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 20 символов")]
         public string Login { get; set; }
+
+        [Required(ErrorMessage = "Поле не может быть пустым")]
+        [Display(Name = "Пароль")]
+        [RegularExpression(@"^[^\s]+$", ErrorMessage = "Некорректный ввод")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 20 символов")]
         public int Password { get; set; }
+
+        [Display(Name = "Роль")]
         public int Role { get; set; }
     }
 }
